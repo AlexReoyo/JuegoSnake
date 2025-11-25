776d4215e1cef3d331b31c0b9b98c73ccbb5e6c5
Alejandro_Reoyo <reoyonavasalex30@gmail.com>
2025-11-25 15:12:16 +0100

    Creación de las clases de Snake y Cuadrado

--- Videojuego/core/src/main/java/com/politecnicomalaga/snake/model/Cuadrado.java
+++ Videojuego/core/src/main/java/com/politecnicomalaga/snake/model/Cuadrado.java
@@ -1,6 +1,56 @@
 package com.politecnicomalaga.snake.model;
 
 public class Cuadrado {
+
+    //atributos - almacenan el estado
+    //por defecto son privados
     private int x,y; //esquina superior izquierda
     private int lado;
+
+    //métodos - implementan el comportamiento con algoritmos
+    //todos los métodos por defecto son public
+
+    //crearse
+    //Al menos un método de creación, se llaman constructores
+    public Cuadrado(int x, int y, int lado){
+        this.x= x; //this es el propio objeto que se está creando
+        this.y= y;
+        this.lado = lado;
+    }
+
+    //getter
+    public int getY() {
+        return y;
+    }
+    public int getX() {
+        return x;
+    }
+    public int getLado() {
+        return lado;
+    }
+
+    //setter
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+    public void setLado(int lado) {
+        this.lado = lado;
+    }
+
+    //más comportamientos
+    public void mover(Snake.Sentido direccion){
+        switch (direccion){
+            case ARR: y-=lado;
+                break;
+            case ABA: y+=lado;
+                break;
+            case DER: x+=lado;
+                break;
+            case IZQ: x-=lado;
+                break;
+        }
+    }
 }
--- Videojuego/core/src/main/java/com/politecnicomalaga/snake/model/Snake.java
+++ Videojuego/core/src/main/java/com/politecnicomalaga/snake/model/Snake.java
@@ -11,4 +11,22 @@ public class Snake {
     private Sentido direccion;
     private Cuadrado[] cuerpo;
     //la cabeza será inicialmente el cuadrado del cuerpo [0]
+
+    //constructor
+    public Snake(int x, int y, int lado, int ancho, int alto){
+        //le paso la posición x de la cabeza y el lado
+        //tenemos siempre al principio dos cuadrados
+        int max_cuadrados = (ancho/lado)*(alto/lado);
+        cuerpo = new Cuadrado[max_cuadrados]; //creación del espacio para el cuerpo
+        num_cuadrados=3;
+        direccion= Sentido.DER;
+        cuerpo[0] = new Cuadrado(x, y, lado);
+        cuerpo[1] = new Cuadrado(x-lado,y,lado);
+        cuerpo[2] = new Cuadrado(x-(lado*2),y,lado);
+    }
+    //getters
+
+    //setters
+
+    //otros métodos
 }
