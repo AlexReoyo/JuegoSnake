8a3c642cc67d0f5e3a0e549e7f6fc2c8e5202469
AlexReoyo <reoyonavasalex30@gmail.com>
2025-12-04 18:56:01 +0100

    Se añaden todas las variables propias de la serpiente y su constructor

--- Videojuego/core/src/main/java/com/politecnicomalaga/snake/model/Snake.java
+++ Videojuego/core/src/main/java/com/politecnicomalaga/snake/model/Snake.java
@@ -11,18 +11,21 @@ public class Snake {
     private Sentido direccion;
     private Cuadrado[] cuerpo;
     //la cabeza será inicialmente el cuadrado del cuerpo [0]
+    private int alto,ancho;
 
     //constructor
-    public Snake(int x, int y, int lado, int ancho, int alto){
+    public Snake(int x, int y, int lado, int ancho, int alto, Texture cabezaDerecha, Texture cuerpoHorizontal){
         //le paso la posición x de la cabeza y el lado
         //tenemos siempre al principio dos cuadrados
         int max_cuadrados = (ancho/lado)*(alto/lado);
         cuerpo = new Cuadrado[max_cuadrados]; //creación del espacio para el cuerpo
         num_cuadrados=3;
         direccion= Sentido.DER;
-        cuerpo[0] = new Cuadrado(x, y, lado);
-        cuerpo[1] = new Cuadrado(x-lado,y,lado);
-        cuerpo[2] = new Cuadrado(x-(lado*2),y,lado);
+        cuerpo[0] = new Cuadrado(x, y, lado, cabezaDerecha);
+        cuerpo[1] = new Cuadrado(x-lado,y,lado, cuerpoHorizontal);
+        cuerpo[2] = new Cuadrado(x-(lado*2),y,lado, cuerpoHorizontal);
+        this.alto=alto;
+        this.ancho=ancho;
     }
     //getters
 
